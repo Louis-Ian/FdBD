508e9cc6f2f318b107f2f84038dc9f8d3c895671
Louis-Ian <louisiangt@gmail.com>
2018-12-15 18:52:56 -0300

    arquivo de povoação

---
+++ Povoa composicoes.sql
@@ -0,0 +1,2 @@
+insert into composicoes values(00001, 'composição para orquestra, em forma de sonata, e que se caracteriza pela multiplicidade de executantes para cada instrumento e pela diversidade de timbres.', 'sinfonia')
+insert into composicoes values(00002, '
